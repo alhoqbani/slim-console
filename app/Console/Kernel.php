208b1dfa623f29c0a474ca710f18f01d27abdf76
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-21 02:59:19 -0700

    Add command generator

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,15 +3,24 @@
 namespace App\Console;
 
 use App\Console\Commands\SayHello;
+use App\Console\Generators\ConsoleGeneratorCommand;
 
 class Kernel
 {
+    
     protected $commands = [
         SayHello::class,
     ];
     
+    protected $defaultCommands = [
+        ConsoleGeneratorCommand::class,
+    ];
+    
     public function getCommands()
     {
-        return $this->commands;
+        return array_merge(
+            $this->commands,
+            $this->defaultCommands
+        );
     }
 }
